6d096def777036bf3de2f2a3dab306ce8c1e532d
dasx10 <dasx10@gmail.com>
2024-07-22 22:41:07 +0300

    update

---
+++ scripts/alias.sh
@@ -0,0 +1,7 @@
+alias nv="nvim"
+alias wge="sudo systemctl status wg-quick@wg0.service"
+alias wgeon="sudo systemctl start wg-quick@wg0.service"
+alias wgeoff="sudo systemctl stop wg-quick@wg0.service"
+alias cl="clear"
+alias seten="setxkbmap -layout us ; sleep 0.5s ; setxkbmap -layout 'us,ua' -option ',winkeys' -option 'grp:alt_shift_toggle'"
+alias d="docker"
